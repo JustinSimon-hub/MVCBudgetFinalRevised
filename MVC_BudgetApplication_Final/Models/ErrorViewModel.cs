ec712069674a90b1cdce1005145042774ba24192
Justin Simon <justinsimon@Justins-MBP.lan>
2024-07-24 02:50:34 -0700

    first commit

---
+++ MVC_BudgetApplication_Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_BudgetApplication_Final.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
