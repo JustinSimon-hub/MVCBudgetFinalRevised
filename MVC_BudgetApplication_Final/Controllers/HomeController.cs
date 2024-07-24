353f5b32e935752bd8faa1b38e712aa10b0b1010
Justin Simon <justinsimon@Justins-MBP.lan>
2024-07-24 03:07:03 -0700

    Removed unessential scaffolding inside models, controllers, and views

--- MVC_BudgetApplication_Final/Controllers/HomeController.cs
+++ MVC_BudgetApplication_Final/Controllers/HomeController.cs
@@ -1,32 +1,21 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MVC_BudgetApplication_Final.Models;
+
 
 namespace MVC_BudgetApplication_Final.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    //The logger method, return view method, and return error page were removed from the scaffold.
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController()
     {
-        _logger = logger;
+        
     }
 
     public IActionResult Index()
     {
         return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
 
--- MVC_BudgetApplication_Final/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVC_BudgetApplication_Final.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
